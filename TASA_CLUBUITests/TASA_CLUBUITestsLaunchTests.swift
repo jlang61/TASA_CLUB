b37135168d0bc5cbfea0229ade0fa765294b1000
Justin Lang <jlang61@ucsb.edu>
2022-09-21 16:41:50 -0700

    Initial Commit

---
+++ TASA_CLUBUITests/TASA_CLUBUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TASA_CLUBUITestsLaunchTests.swift
+//  TASA_CLUBUITests
+//
+//  Created by Justin Lang on 9/21/22.
+//
+
+import XCTest
+
+class TASA_CLUBUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
